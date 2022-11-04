fe2c759c6e6041df2cbe62147bc819ff2c640de7
Martijn Zentjens <martijnzentjens@hotmail.com>
2022-11-04 10:41:05 +0100

    feat(prettier): Added prettier to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.{j,t}s?(x)"],
+      env: {
+        jest: true,
+      },
     },
-    extends: [
-        "plugin:vue/recommended",
-        "eslint:recommended",
-        "@vue/typescript/recommended",
-    ],
-    parserOptions: {
-        ecmaVersion: 2020,
-        parser: "@typescript-eslint/parser",
-    },
-    rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "linebreak-style": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-    },
-    overrides: [
-        {
-            files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.{j,t}s?(x)"],
-            env: {
-                jest: true,
-            },
-        },
-    ],
+  ],
 };
